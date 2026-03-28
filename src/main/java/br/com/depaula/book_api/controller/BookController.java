3b393df9d01f1ab7e8510da294ca3624b949c133
Guilherme23645 <gui.depaula236@gmail.com>
2026-03-27 21:37:44 -0300

    feat: add REST route controller for Book

---
+++ src/main/java/br/com/depaula/book_api/controller/BookController.java
@@ -0,0 +1,53 @@
+package br.com.depaula.book_api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.depaula.book_api.model.Book;
+import br.com.depaula.book_api.service.BookService;
+
+@RestController
+@RequestMapping("books")
+public class BookController {
+
+    private final BookService service;
+
+    public BookController(BookService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> addBook(@RequestBody Book book) {
+        service.addBook(book);
+        return ResponseEntity.ok(book);
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<Book>> findAllBooks() {
+        return ResponseEntity.ok(service.findAllBooks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> findBookById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findBookById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBookById(@PathVariable Long id, @RequestBody Book book) {
+        service.updateBookById(id, book);
+        return ResponseEntity.ok(book);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBookById(@PathVariable Long id) {
+        service.deleteBookById(id);
+        return ResponseEntity.ok().build();
+    }
+}
