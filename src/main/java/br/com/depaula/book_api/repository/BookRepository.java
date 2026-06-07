fa3ec8aaf66882593ba2f109b6511ef4beec5eed
Guilherme Henrique de Paula <euamojaguatirica@gmail.com>
2026-06-06 23:00:47 -0300

    fix: update import

--- src/main/java/br/com/depaula/book_api/repository/BookRepository.java
+++ src/main/java/br/com/depaula/book_api/repository/BookRepository.java
@@ -1,6 +1,6 @@
 package br.com.depaula.book_api.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.depaula.book_api.model.Book;
