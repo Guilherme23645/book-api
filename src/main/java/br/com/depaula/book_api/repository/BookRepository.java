71a04749d98f1afe37075eda1fc319ac4a6b697e
Guilherme Henrique de Paula <euamojaguatirica@gmail.com>
2026-06-06 22:59:19 -0300

    feat: change BookRepository to extend JpaRepository

--- src/main/java/br/com/depaula/book_api/repository/BookRepository.java
+++ src/main/java/br/com/depaula/book_api/repository/BookRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import br.com.depaula.book_api.model.Book;
 
 @Repository
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
